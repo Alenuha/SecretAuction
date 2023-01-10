6c0cd6311a4501c8d83485dee1837121bc1188b3
Alenuha <85442504+Alenuha@users.noreply.github.com>
2023-01-10 11:16:41 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from replit import clear
+
+
+import art
+print(art.logo)
+
+bids = {}
+bidding_finished = False
+
+def highest_bidder(bidingl):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidingl:
+        bid_amount = bidingl[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with ${highest_bid}")
+
+while not bidding_finished:
+    name = input("What is your name?\n")
+    price = input("What is your bid? $\n")
+    if type(price) != int:
+        price = int(input("Please use numbers only!\n What is your bid? $\n"))
+    else:
+        price = int(price)
+
+    bids[name] = price
+    keep_going = input("Are there any other bidders? Type 'yes' or 'no'\n").lower()
+    if keep_going == 'no':
+        bidding_finished = True
+        highest_bidder(bids)
+    elif keep_going == 'yes':
+        clear()
+
+
